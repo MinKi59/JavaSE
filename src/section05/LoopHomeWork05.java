78bb7f3704d933be7cfb2fe965e9e394c70d48f3
권민기 <@DESKTOP-GOMEVLO>
2023-09-19 21:11:59 +0900

    LOOP

--- src/section05/LoopHomeWork05.java
+++ src/section05/LoopHomeWork05.java
@@ -4,10 +4,8 @@ public class LoopHomeWork05 {
 	public static void main(String[] args) {
 		for (int i = 2; i < 10	; i += 3) {
 			for (int j = 1; j < 10; j++) {
-				System.out.print(i + "X" + j + "=" + (i * j));
-				System.out.print("	");
-				System.out.print((i + 1) + "X" + j + "=" + ((i + 1) * j));
-				System.out.print("	");
+				System.out.print(i + "X" + j + "=" + (i * j) + "	");
+				System.out.print((i + 1) + "X" + j + "=" + ((i + 1) * j) + "	");
 				if (i != 8) {
 					System.out.print((i + 2) + "X" + j + "=" + ((i + 2) * j));
 					System.out.println();
